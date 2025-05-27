7d91feea09d9ef15c81380a311bb8a4d1c76d861
P0pini <popiksonek@gmail.com>
2025-05-27 15:25:17 +0200

    aa

--- src/main/java/org/myshop/Song.java
+++ src/main/java/org/myshop/Song.java
@@ -1,13 +1,15 @@
 package org.myshop;
 
+import pl.umcs.oop.database.DatabaseConnection;
+
 import java.sql.PreparedStatement;
 import java.util.Optional;
 import java.sql.SQLException;
-import org.exapmle.database.DatabaseConnection;
+
 
 public record Song(String artist, String title, int duration) {
     public static class Persistence{
-        public static Optional<Song> read(int index){
+        public static Optional<Song> read(int index) throws  SQLException{
             String sql="SELECT * FROM song WHERE id=?;";
             PreparedStatement stmt = DatabaseConnection.getConnection().prepareStatement(sql);
         }
